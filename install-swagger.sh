7e95b711736394119011401946e8d6caf414d8d7
Jon Alfaro <jon.alfaro@vorteil.io>
2021-10-12 15:24:59 +1000

    update
    Signed-off-by: Jon Alfaro <jon.alfaro@vorteil.io>

--- install-swagger.sh
+++ install-swagger.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dir=$(mktemp -d) 
-git clone https://github.com/go-swagger/go-swagger "$dir" 
-cd "$dir"
-go install ./cmd/swagger
+download_url=$(curl -s https://api.github.com/repos/go-swagger/go-swagger/releases/latest | \
+  jq -r '.assets[] | select(.name | contains("'"$(uname | tr '[:upper:]' '[:lower:]')"'_amd64")) | .browser_download_url')
+curl -o /usr/local/bin/swagger -L'#' "$download_url"
+chmod +x /usr/local/bin/swagger
